0ea95e6ce1138e74a116467f2215deff321f5da9
Michael Calewaert <michael.calewaert@codit.eu>
2021-08-09 07:25:03 +0200

    docs: Prefix version numbers with "v" (#75)
    * :sparkles: add Docusaurus (#1)
    
    * :hammer: remove broken links + update config
    
    * :art: update sidebar config + github repo
    
    * :hammer: update footer
    
    * :lipstick: theming
    
    * :art: add logo
    
    * :art: update logos
    
    * :lipstick: Footer styling updates
    
    * :bento: Add favicon
    
    * :sparkles: use dev config file
    
    * :green_heart: env variables (#2)
    
    * Auto stash before merge of "master" and "origin/master"
    
    * :memo: Add documentation
    
    * :ok_hand: change logo
    
    * :lipstick: update dark theme colors
    
    * :sparkles: seo image
    
    * :fire: remove docs from root
    
    * :fire: restore src files
    
    * :fire: undo .github files removal
    
    * :sparkles: update preview docs + github references
    
    * :hammer: fix broken link
    
    * :lipstick: alert background fix
    
    * :ok_hand: rename docs to documentation
    
    * :ok_hand: add support for csharp
    
    * :ok_hand: update organisation name
    
    * :ok_hand: update URL
    
    * :ok_hand: rename docs to next
    
    * :ok_hand: update README
    
    * :ok_hand: rename unreleased docs folder to preview
    
    * :fire: remove unused images
    
    * :ok_hand: add netlify.toml
    
    * Change Netlify name
    
    * :sparkles: Update index page title + prefix versions with v
    
    * :fire: remove old versions
    
    * :ok_hand: change favicon
    
    * :ok_hand: Setup v0.1 + edit url fix
    
    * :ok_hand: Fix wrong link v0.1
    
    Co-authored-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -8,7 +8,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
+  favicon: 'img/arcus.png',
   organizationName: 'arcus-azure', // Usually your GitHub org/user name.
   projectName: 'Arcus Background Jobs', // Usually your repo name.
   themeConfig: {
@@ -75,7 +75,7 @@ module.exports = {
           path: 'preview',
           // Please change this to your repo.
           editUrl:
-            'https://github.com/arcus-azure/arcus.backgroundjobs/edit/master',
+            'https://github.com/arcus-azure/arcus.backgroundjobs/edit/master/docs',
           includeCurrentVersion:process.env.CONTEXT !== 'production',
         },
         theme: {
