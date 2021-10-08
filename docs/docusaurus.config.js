213b04c9e915f3a71d0fbf51d577d5abec4f01d7
Michael Calewaert <michael.calewaert@codit.eu>
2021-10-08 17:08:28 +0200

    :ok_hand: use env variable

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
   projectName: 'Arcus Background Jobs', // Usually your repo name.
   themeConfig: {
     algolia: {
-      apiKey: 'fdbac718aeb246ac60aeae5bd286cbbf',
+      apiKey: process.env.ALGOLIA_API_KEY,
       indexName: 'arcus-azure',
       contextualSearch: true,
       searchParameters: {
