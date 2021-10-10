d22a6a030f49f8a6e7a89464db6bca9206afc18d
Pim Simons <pim.simons@codit.eu>
2021-10-10 17:33:33 +0200

    added basic unit tests for the azuread clientsecret backgroundjob

---
+++ src/Arcus.BackgroundJobs.Tests.Unit/AzureAD/ClientSecretExpirationInfoProviderTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Arcus.BackgroundJobs.AzureAD.ClientSecretExpiration;
+using Bogus;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Graph;
+using Xunit;
+
+namespace Arcus.BackgroundJobs.Tests.Unit.AzureAD.ClientSecretExpiration
+{
+    [Trait(name: "Category", value: "Unit")]
+    public class ClientSecretExpirationInfoProviderTests
+    {
+        private static readonly Faker BogusGenerator = new Faker();
+
+        [Fact]
+        public void CreateClientSecretExpirationInfoProvider_WithNullGraphServiceClient_Throws()
+        {
+            // Assert
+            Assert.ThrowsAny<ArgumentException>(() => new ClientSecretExpirationInfoProvider(null, NullLogger.Instance));
+        }
+
+        [Fact]
+        public void CreateClientSecretExpirationInfoProvider_WithNullLogger_Throws()
+        {
+            // Arrange
+            GraphServiceClient client = new GraphServiceClient(
+                new DelegateAuthenticationProvider((requestMessage) =>
+                {
+                    requestMessage
+                    .Headers
+                    .Authorization = new AuthenticationHeaderValue("bearer", BogusGenerator.Random.ToString());
+
+                    return Task.CompletedTask;
+                })
+            );
+
+            // Assert
+            Assert.ThrowsAny<ArgumentException>(() => new ClientSecretExpirationInfoProvider(client, null));
+        }
+
+        [Fact]
+        public async Task GetApplicationsWithExpiredAndAboutToExpireSecrets_WithExpirationThresholdLessThanZero_Throws()
+        {
+            // Arrange
+            int expirationThreshold = -1;
+            GraphServiceClient client = new GraphServiceClient(
+                new DelegateAuthenticationProvider((requestMessage) =>
+                {
+                    requestMessage
+                    .Headers
+                    .Authorization = new AuthenticationHeaderValue("bearer", BogusGenerator.Random.ToString());
+
+                    return Task.CompletedTask;
+                })
+            );
+            var provider = new ClientSecretExpirationInfoProvider(client, NullLogger.Instance);
+
+            // Act / Assert
+            await Assert.ThrowsAnyAsync<ArgumentException>(() => provider.GetApplicationsWithExpiredAndAboutToExpireSecrets(expirationThreshold));
+        }
+    }
+}
