ce732ad75824500383f3664fc50d1477959586c6
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2021-11-16 08:45:07 +0100

    pr-sug: non expiration date = valid forever

--- src/Arcus.BackgroundJobs.AzureActiveDirectory/ClientSecretExpirationInfoProvider.cs
+++ src/Arcus.BackgroundJobs.AzureActiveDirectory/ClientSecretExpirationInfoProvider.cs
@@ -35,52 +35,50 @@ namespace Arcus.BackgroundJobs.AzureActiveDirectory
         /// <summary>
         /// Returns a list of applications that have secrets that have already expired or are about to expire.
         /// </summary>
-        /// <param name="expirationThreshold">The threshold for the expiration, if the end datetime for a secret is lower than this value a <see cref="CloudEvent"/> will be published.</param>
+        /// <param name="expirationThresholdInDays">The threshold for the expiration, if the end datetime for a secret is lower than this value a <see cref="CloudEvent"/> will be published.</param>
         /// <returns>A list of applications that have expired secrets or secrets that are about to expire.</returns>
-        /// <exception cref="ArgumentOutOfRangeException">Thrown when the <paramref name="expirationThreshold"/> is less than zero.</exception>
-        public async Task<IEnumerable<AzureApplication>> GetApplicationsWithPotentialExpiredSecrets(int expirationThreshold)
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the <paramref name="expirationThresholdInDays"/> is less than zero.</exception>
+        public async Task<IEnumerable<AzureApplication>> GetApplicationsWithPotentialExpiredSecrets(int expirationThresholdInDays)
         {
-            Guard.NotLessThan(expirationThreshold, 0, nameof(expirationThreshold), "Requires an expiration threshold in maximum remaining days the secrets are allowed to stay active");
+            Guard.NotLessThan(expirationThresholdInDays, 0, nameof(expirationThresholdInDays), "Requires an expiration threshold in maximum remaining days the secrets are allowed to stay active");
 
             var applicationsList = new List<AzureApplication>();
             IGraphServiceApplicationsCollectionPage applications = await _graphServiceClient.Applications.Request().GetAsync();
+            Application[] applicationsWithSecrets = applications.Where(app => app.PasswordCredentials?.Any() is true).ToArray();
 
-            foreach (Application application in applications)
+            foreach (Application application in applicationsWithSecrets)
             {
-                if (application.PasswordCredentials != null && application.PasswordCredentials.Any())
+                foreach (PasswordCredential passwordCredential in application.PasswordCredentials)
                 {
-                    foreach (PasswordCredential passwordCredential in application.PasswordCredentials)
-                    {
-                        string applicationName = application.DisplayName;
-                        Guid? keyId = passwordCredential.KeyId;
-                        double remainingValidDays = DetermineRemainingDaysBeforeExpiration(passwordCredential);
+                    string applicationName = application.DisplayName;
+                    Guid? keyId = passwordCredential.KeyId;
 
-                        if (remainingValidDays <= expirationThreshold)
+                    if (passwordCredential.EndDateTime.HasValue)
+                    {
+                        double remainingValidDays = DetermineRemainingDaysBeforeExpiration(passwordCredential.EndDateTime.Value);
+                        if (remainingValidDays <= expirationThresholdInDays)
                         {
                             applicationsList.Add(new AzureApplication(applicationName, keyId, passwordCredential.EndDateTime, remainingValidDays));
-                            _logger.LogInformation("The secret {KeyId} for application {ApplicationName} has an expired secret or a secret that will expire within {ExpirationThresholdInDays} days", keyId, applicationName, expirationThreshold);
+                            _logger.LogInformation("The secret {KeyId} for application {ApplicationName} has an expired secret or a secret that will expire within {ExpirationThresholdInDays} days", keyId, applicationName, expirationThresholdInDays);
                         }
                         else
                         {
                             _logger.LogTrace("The secret {KeyId} for application {ApplicationName} is still valid", keyId, applicationName);
                         }
                     }
+                    else
+                    {
+                        _logger.LogTrace("The secret {KeyId} for application {ApplicationName} has no expiration date", keyId, applicationName);
+                    }
                 }
             }
 
-            return applicationsList;
+            return applicationsList.ToArray();
         }
 
-        private static double DetermineRemainingDaysBeforeExpiration(PasswordCredential password)
+        private static double DetermineRemainingDaysBeforeExpiration(DateTimeOffset expirationDate)
         {
-            DateTimeOffset? expirationDate = password.EndDateTime;
-            if (expirationDate is null)
-            {
-                return 0;
-            }
-            
-            TimeSpan remainingTime = expirationDate.Value - DateTimeOffset.UtcNow;
-
+            TimeSpan remainingTime = expirationDate - DateTimeOffset.UtcNow;
             return remainingTime.TotalDays;
         }
     }
