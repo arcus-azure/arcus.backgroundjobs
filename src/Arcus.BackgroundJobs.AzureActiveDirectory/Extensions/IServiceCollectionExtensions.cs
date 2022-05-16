3c4903a28be356a25ad7b52b16a5a66939758f57
stijnmoreels <9039753+stijnmoreels@users.noreply.github.com>
2022-05-16 10:03:03 +0200

    pr-fix: follow-up merge w/ 'master'

--- src/Arcus.BackgroundJobs.AzureActiveDirectory/Extensions/IServiceCollectionExtensions.cs
+++ src/Arcus.BackgroundJobs.AzureActiveDirectory/Extensions/IServiceCollectionExtensions.cs
@@ -20,22 +20,6 @@ namespace Microsoft.Extensions.DependencyInjection
         /// which will query Azure Active Directory for applications that have expired or soon to be expired secrets and send a CloudEvent to an Event Grid Topic.
         /// </summary>
         /// <remarks>
-        ///     Make sure that the application has an Arcus EventGrid publisher configured.
-        ///     For on the Arcus secret store: <a href="https://eventgrid.arcus-azure.net/Features/publishing-events" />.
-        /// </remarks>
-        /// <param name="services">The services to add the background job to.</param>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is <c>null</c>.</exception>
-        public static IServiceCollection AddClientSecretExpirationJob(this IServiceCollection services)
-        {
-            Guard.NotNull(services, nameof(services));
-            return AddClientSecretExpirationJob(services, configureOptions: null);
-        }
-
-        /// <summary>
-        /// Adds the <see cref="ClientSecretExpirationJob"/> scheduled job
-        /// which will query Azure Active Directory for applications that have expired or soon to be expired secrets and send a CloudEvent to an Event Grid Topic.
-        /// </summary>
-        /// <remarks>
         ///     Make sure that you register an <see cref="IEventGridPublisher"/> instance that the background job can use to publish events for potential expired Azure Application secrets.
         ///     For more information on Azure EventGrid, see: <a href="https://eventgrid.arcus-azure.net/Features/publishing-events" />.
         /// </remarks>
