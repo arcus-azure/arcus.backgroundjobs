16037c5c2568091a3675e84fc91b507e731b40fa
Stijn Moreels <9039753+stijnmoreels@users.noreply.github.com>
2022-12-08 17:47:06 +0100

    Update docusaurus.build.config.js (#173)

--- docs/docusaurus.build.config.js
+++ docs/docusaurus.build.config.js
@@ -5,6 +5,7 @@ module.exports = {
   themeConfig: {
     ...buildConfig.themeConfig,
     algolia: {
+      appId: process.env.ALGOLIA_APP_ID,
       apiKey: process.env.ALGOLIA_API_KEY,
       indexName: 'arcus-azure',
       contextualSearch: true,
